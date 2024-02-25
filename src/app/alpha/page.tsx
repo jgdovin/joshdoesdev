5a49a605de8c0d3121ff5e65f06c45657f3051d3
Josh Gdovin <joshgdovin@gmail.com>
2024-02-24 18:45:32 -0700

    articles

--- src/app/alpha/page.tsx
+++ src/app/alpha/page.tsx
@@ -3,9 +3,25 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-24 gap-4">
-      <Link href="/">To Home</Link>
-      <div><Button variant="default">Test</Button></div>
+    <main className="flex flex-col items-center justify-center py-12 gap-4">
+      <div className="relative flex flex-col gap-6 w-full">
+        <article className="scale-100 hover:scale-105 transition-all">
+          <Link href="/">
+            <h2>Some Article</h2>
+            <p className="text-[13px]">January, 10th, 2024</p>
+            <p className="mt-2">Test description</p>
+          </Link>
+
+        </article>
+        <article className="scale-100 hover:scale-105 transition-all">
+          <Link href="/">
+            <h2>Some Article</h2>
+            <p className="text-[13px]">January, 10th, 2024</p>
+            <p className="mt-2">Test description</p>
+          </Link>
+
+        </article>
+      </div>
     </main>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,8 @@ import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 const Header = () => {  
   return (
-    <header className="flex flex-row place-content-between mb-14">
-      <span>JoshDoes.Dev</span>
+    <header className="flex flex-row place-content-between mb-2">
+      <h2 className="bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text">JoshDoes.Dev</h2>
       <ThemeSwitcher />
     </header>
   )
