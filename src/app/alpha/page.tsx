16e7d1573398404aefb634446ebd6bc295de3d5c
Josh Gdovin <joshgdovin@gmail.com>
2024-02-24 17:04:30 -0700

    smaller page

--- src/app/alpha/page.tsx
+++ src/app/alpha/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
+    <main className="flex flex-col items-center justify-center p-24 gap-4">
       <Link href="/">To Home</Link>
       <div><Button variant="default">Test</Button></div>
     </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <Link href="/alpha">To Alpha Page</Link>
       <Image src="/under_construction.webp" width={800} height={800} alt="Under Construction!" />
     </main>
