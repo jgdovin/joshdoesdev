0484d1b220133d5a461eba3021ecd950aabb0d78
Josh Gdovin <joshgdovin@gmail.com>
2024-02-25 17:05:02 -0700

    icons n stuff

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,13 +3,15 @@ import Switch from '@/components/ui/Switch';
 import Label from '@/components/ui/Label';
 import { useThemeContext } from '@/contexts/ThemeProvider';
 
+import { MoonStar, Sun } from 'lucide-react';
+
 const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useThemeContext();
   
   return (
     <div className="flex items-center space-x-2">
       <Switch id="theme-switch" checked={theme === "dark"} onClick={() => toggleTheme()} />
-      <Label htmlFor="theme-switch">{theme}</Label>
+      <Label htmlFor="theme-switch">{theme === "dark" ? <MoonStar /> : <Sun />}</Label>
     </div>
   )
 };
