393fb8bd2dab693275c6be3f7af8f6892053ce73
Josh Gdovin <joshgdovin@gmail.com>
2024-04-09 20:33:01 -0600

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Header from "@/components/header";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata: Metadata = {
   title: "Josh Does Dev",
   description: "Home for all things I do",
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="mx-auto max-w-lg px-5 py-12 md:max-w-2xl lg:max-w-5xl">
+        <Analytics />
         <ThemeProvider enableSystem attribute="class">
           <Header />
           {children}
