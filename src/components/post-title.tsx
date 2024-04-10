1a41351d37c8d19364890c52f3364a4327bc69fc
Josh Gdovin <joshgdovin@gmail.com>
2024-04-10 09:07:59 -0600

    add paddig to title of posts

--- src/components/post-title.tsx
+++ src/components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostTitle({ children }: Props) {
   return (
-    <h1 className="my-12 bg-gradient-to-r from-accent to-destructive bg-clip-text p-1 text-center text-xl font-bold leading-tight tracking-tight text-transparent dark:to-muted md:text-left md:text-xl md:leading-none lg:text-7xl">
+    <h1 className="my-12 bg-gradient-to-r from-accent to-destructive bg-clip-text p-3 text-center text-xl font-bold leading-tight tracking-tight text-transparent dark:to-muted md:text-left md:text-xl md:leading-none lg:text-7xl">
       {children}
     </h1>
   );
