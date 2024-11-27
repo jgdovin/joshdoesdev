51ae19556ca12fcf3c58a1a7a57f949c281400ca
Josh Gdovin <joshgdovin@gmail.com>
2024-11-27 13:34:06 -0700

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,17 +67,17 @@ export default function Home() {
           </CardHeader>
           <CardContent className="flex flex-col gap-6">
             <p>
-              Hi, I'm Josh Gdovin—a seasoned DevOps professional with a passion
-              for implementing best practices and enhancing the developer
-              experience.
+              Hi, I&apos;m Josh Gdovin—a seasoned Web Application Engineer &
+              DevOps professional with a passion for implementing best practices
+              and enhancing the developer experience.
             </p>
             <p>
               With a background in cloud architecture and infrastructure
               automation, I strive to build scalable and efficient systems.
             </p>
             <p>
-              Outside of work, I'm an avid hiker, snowboarder, and disc golf
-              enthusiast. I also have a deep love for animals, especially
+              Outside of work, I&apos;m an avid hiker, snowboarder, and disc
+              golf enthusiast. I also have a deep love for animals, especially
               ferrets and cats. Welcome to my blog, where I share insights on
               Technology, Devops, Engineering and more!
             </p>
