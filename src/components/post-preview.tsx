69ed84d81683d99ad6ed829b439e252a3c92069a
Josh Gdovin <joshgdovin@gmail.com>
2024-11-27 11:22:20 -0700

    update layout a bit

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { oswaldBold, raleway } from "@/lib/fonts";
 
 export type PostPreviewType = {
   title: string;
@@ -12,12 +13,17 @@ const PostPreview = (postPreview: PostPreviewType) => {
   const { title, date, description, slug } = postPreview;
   return (
     <article className="scale-100 transition-all hover:scale-105">
-      <Link href={`/posts/${slug}`}>
-        <h2 className="bg-gradient-to-r from-accent to-destructive bg-clip-text text-lg text-transparent dark:to-muted md:text-2xl lg:text-3xl">
+      <Link href={`/posts/${slug}`} className="flex flex-col gap-5">
+        <h2
+          className={`bg-gradient-to-r from-accent to-destructive bg-clip-text text-lg text-transparent dark:to-muted md:text-2xl lg:text-3xl ${oswaldBold.className} p-1`}
+        >
           {title}
         </h2>
-        <span className="mt-[-10px] pl-2 text-xs text-gray-300">{date}</span>
-        <p className="mt-2 text-xs md:text-base">{description}</p>
+        <span className="mt-[-10px] pl-2 text-xs text-gray-400">{date}</span>
+        <p className={`mt-2 pl-10 text-sm ${raleway.className} text-gray-500`}>
+          {description}
+        </p>
+        <p className="self-center">Read More...</p>
       </Link>
     </article>
   );
