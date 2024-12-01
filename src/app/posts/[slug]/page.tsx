d49898ec406360ad10e39a9095ef7656721d6f95
Josh Gdovin <joshgdovin@gmail.com>
2024-12-01 12:27:52 -0700

    add proper tags to individual posts

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -22,7 +22,11 @@ export default async function Post({ params }: Params) {
     <main className="mx-auto max-w-4xl">
       <article className="overflow-hidden rounded-lg bg-white shadow-lg dark:bg-gray-800">
         <div className="p-8">
-          <Badge className="mb-4">Devops</Badge>
+          {post.tags.split(",").map((tag) => (
+            <Badge key={tag} variant="secondary" className="mr-2">
+              {tag}
+            </Badge>
+          ))}
           <h1 className="mb-4 text-4xl font-bold">{post.title}</h1>
           <div className="mb-6 flex items-center text-gray-500 dark:text-gray-400">
             <User size={18} className="mr-2" />
