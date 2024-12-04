13e2b1b42d5cb1399656b8a4698486501f66f4d2
Josh Gdovin <joshgdovin@gmail.com>
2024-12-04 15:59:46 -0700

    fix css

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { ThemeProvider } from "next-themes";
 
 import { Analytics } from "@vercel/analytics/react";
